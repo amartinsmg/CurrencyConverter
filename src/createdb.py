06d602366737e35e037fc55f7ecf92773d27be0b
amartinsmg <andremindurimg@gmail.com>
2022-07-19 21:54:32 -0300

    Update program and createdb

--- src/createdb.py
+++ src/createdb.py
@@ -1,7 +1,6 @@
 from os import mkdir, path
 import requests
 import sqlite3
-import xmltodict
 
 
 def main():
@@ -11,18 +10,19 @@ def main():
 	cur = db.cursor()
 	cur.execute('''CREATE TABLE currencies_table(
 		id   INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT,
-		code TEXT
+		code TEXT,
+		name TEXT
 	);''')
-	query = "INSERT INTO currencies_table(name, code) VALUES('{}', '{}');"
-	response = requests.post('https://supply-xml.booking.com/hotels/xml/currencies')
-	currencies = xmltodict.parse(response.content)['currencies']['currency']
+	query = "INSERT INTO currencies_table(code, name) VALUES('{}', '{}');"
+	response = requests.post('https://openexchangerates.org/api/currencies.json')
+	currencies = response.json()
 
 	for currency in currencies:
-		current_query = query.format(currency['@name'], currency['@currencycode'])
+		current_query = query.format(currency, currencies[currency].replace("\'", ''))
 		cur.execute(current_query)
 
 	db.commit()
+
 
 if __name__ == '__main__':
 	main()
--- src/program.py
+++ src/program.py
@@ -1,49 +1,61 @@
+from tkinter.ttk import Combobox
 import requests
+import sqlite3
+from os import path
 from tkinter import Button, Entry, Frame, Label, Tk
+from ttkwidgets.autocomplete import AutocompleteEntry
+
+db_path = f'{path.dirname(__file__)}/../db/'
+db = sqlite3.connect(f'{db_path}/currencies.db')
+cur = db.cursor()
 
 
 def currency_exchange(input_currency, output_currency):
-	response = requests.get(
-		f'https://economia.awesomeapi.com.br/json/last/{input_currency}-{output_currency}'
-	)
-	response_dict = response.json()
-	return float(response_dict["ask"])
+    response = requests.get(
+        f'https://economia.awesomeapi.com.br/json/last/{input_currency}-{output_currency}'
+    )
+    response_dict = response.json()
+    return float(response_dict["ask"])
 
 
 class Root(Tk):
 
-	def __init__(self):
-		super(Root, self).__init__()
+    def __init__(self):
+        super(Root, self).__init__()
 
-		self.title("Currency Coverter")
-		self.minsize(600, 450)
+        self.title("Currency Coverter")
+        self.minsize(600, 450)
 
-		font = ('arial', 13, 'normal')
-		frame = Frame(self)
-		frame.pack(expand=True)
-		l1 = Label(frame, text='Convert from: ', font=font)
-		e1 = Entry(frame, font=font)
-		l2 = Label(frame, text='To: ', font=font)
-		e2 = Entry(frame, font=font)
-		l3 = Label(frame, text='Value: ', justify='left', font=font)
-		btn = Button(frame, text='Calculate', font=font, bg='#00d7ff')
-		e3 = Entry(frame, font=font)
-		l4 = Label(frame, text='Result: ', font=font)
-		result = Label(frame, font=font)
-		result['text'] = str(5+5)
-		result['text'] = str(5+7)
-		l1.grid(row=0, column=0, pady=4)
-		e1.grid(row=0, column=1, ipadx=2, ipady=2, pady=4)
-		l2.grid(row=1, column=0, pady=4)
-		e2.grid(row=1, column=1, ipadx=2, ipady=2, pady=4)
-		l3.grid(row=2, column=0, pady=4)
-		e3.grid(row=2, column=1, ipadx=2, ipady=2, pady=4)
-		btn.grid(row=3, column=1, pady=(10, 4))
-		l4.grid(row=4,column=0, pady=(20, 4))
-		result.grid(row=4,column=1, pady=(20, 4))
+        cur.execute('SELECT name FROM currencies_table')
+        dabase_rows = cur.fetchall()
+        currencies_names = [row[0] for row in dabase_rows]
+        font = ('arial', 13, 'normal')
+        frame = Frame(self)
+        frame.pack(expand=True)
+        l1 = Label(frame, text='Convert from: ', font=font, justify='left')
+        e1 = AutocompleteEntry(frame, completevalues=currencies_names, font=font)
+        l2 = Label(frame, text='To: ', font=font, justify='left')
+        e2 = AutocompleteEntry(frame, completevalues=currencies_names, font=font)
+        l3 = Label(frame, text='Value: ', font=font, justify='left')
+        btn = Button(frame, text='Calculate', font=font, bg='#b3b3b3')
+        e3 = Entry(frame, font=font)
+        l4 = Label(frame, text='Result: ', font=font)
+        result = Label(frame, font=font)
+        
+        result['text'] = str(5 + 5)
+        result['text'] = str(5 + 7)
+        l1.grid(row=0, column=0, pady=4)
+        e1.grid(row=0, column=1, ipadx=2, ipady=2, pady=4)
+        l2.grid(row=1, column=0, pady=4)
+        e2.grid(row=1, column=1, ipadx=2, ipady=2, pady=4)
+        l3.grid(row=2, column=0, pady=4)
+        e3.grid(row=2, column=1, ipadx=2, ipady=2, pady=4)
+        btn.grid(row=3, column=1, pady=(10, 4))
+        l4.grid(row=4, column=0, pady=(20, 4))
+        result.grid(row=4, column=1, pady=(20, 4))
 
 
 window = Root()
 
 if __name__ == "__main__":
-	window.mainloop()
+    window.mainloop()
